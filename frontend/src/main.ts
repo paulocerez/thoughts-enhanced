b5b340f2fdf31a416eea50d9d629e47452fc50bd
paulocerez <paulo.ramirez@web.de>
2023-04-25 11:51:57 +0200

    fix: nodemon issues/live reloading

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,12 +2,13 @@
 import { createApp } from 'vue'
 // import the root component App from a single-file component -> contains other components as its children
 import App from './App.vue'
+import router from './router'
+import './style.css'
+
 const app = createApp(App)
 
-import './style.css'
-// imports index.ts automatically without specification like router/index
-import router from './router'
 app.use(router)
+// imports index.ts automatically without specification like router/index
 // import store from './store'
 // app.use(store)
 // .mount()-method makes the application instance render -> expects a container element (either DOM element or selector string)
