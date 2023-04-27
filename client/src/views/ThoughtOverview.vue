eb0141c302caabf1df36145db328492195a4f6b0
paulocerez <paulo.ramirez@web.de>
2023-04-27 21:14:31 +0200

    feat: dist-folder

---
+++ client/src/views/ThoughtOverview.vue
@@ -0,0 +1,11 @@
+<script lang="ts">
+export default {
+	data() {
+	}
+}
+
+</script>
+
+<template></template>
+
+<style></style>
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -4,6 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    outDir: 'dist', // specify the output directory
+    emptyOutDir: true // empty the output directory before building
+  },
   server: {
     watch: {
 	// usePolling -> watch in file system for changes at a regular interval, works across different operating systems
