7eac21e9dd99c3c340aa1fd7a86a67dbe72439df
paulocerez <paulo.ramirez@web.de>
2023-06-20 10:51:46 +0200

    fix: rebuild history view

--- src/views/ThoughtHistory.vue
+++ src/views/ThoughtHistory.vue
@@ -3,56 +3,56 @@
 		<h2 class="text-2xl -mt-64 font-bold mb-4">My Entries</h2>
 		<div v-if="loading" class="text-center">Loading...</div>
 		<div v-else>
-			<post-item v-for="post in posts" :key="post.id" :post="post" @delete-post="deletePost"
-				@edit-post="editPost"></post-item>
+			<!-- create a post element for every post in the post database table -->
+			<div v-for="post in posts" :key="post.id">
+				<h3 class="font-bold text-lg mb-2">{{ post.title }}</h3>
+				<h5 class="font-bold text-lg mb-2">{{ post.category }}</h5>
+				<p class="text-gray-700 text-base">{{ post.thought }}</p>
+			</div>
 		</div>
 	</div>
 </template>
-  
 <script lang="ts">
-import PostItem from '../components/PostItem.vue'
-import { ref } from "vue";
+import { defineComponent, ref } from "vue";
 import { useRouter } from "vue-router";
-import axios from 'axios'
-import backendUrl from '../../config'
+import axios from 'axios';
+import backendUrl from '../../config';
 
 const router = useRouter();
 
-export default {
-	components: {
-		PostItem,
-	},
+export default defineComponent({
 	data() {
+		// reset to default values
 		return {
 			posts: [] as { id: number; title: string; category: string; thought: string }[],
 			loading: true,
-		}
+		};
 	},
 	mounted() {
-		this.fetchPosts()
+		this.fetchPosts();
 	},
 	methods: {
 		async fetchPosts() {
 			try {
-				const response = await axios.get(`${backendUrl}/api/posts`)
-				this.posts = response.data
-				this.loading = false
+				const response = await axios.get<{ id: number; title: string; category: string; thought: string }[]>(`${backendUrl}/api/posts/all`);
+				this.posts = response.data;
+				this.loading = false;
 			} catch (error) {
-				console.error(error)
+				console.error(error);
 			}
 		},
-		async deletePost(postId: number) {
-			try {
-				await axios.delete(`${backendUrl}/api/posts/${postId}`)
-				this.posts = this.posts.filter(post => post.id !== postId)
-			} catch (error) {
-				console.error(error)
-			}
-		},
-		editPost(postId: number) {
-			router.push({ name: 'edit', params: { postId: postId.toString() } })
-		}
+		// async deletePost(postId: number) {
+		//   try {
+		//     await axios.delete(`${backendUrl}/api/posts/${postId}`);
+		//     this.posts = this.posts.filter(post => post.id !== postId);
+		//   } catch (error) {
+		//     console.error(error);
+		//   }
+		// },
+		// editPost(postId: number) {
+		//   router.push({ name: 'edit', params: { postId: postId.toString() } });
+		// }
 	},
-}
+});
 </script>
   
